b1e0f730514ed65bc2d4b7ed9d9d41d8b4629640
borcheTrumpeski <115028528+borcheTrumpeski@users.noreply.github.com>
2022-10-06 12:57:34 +0200

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { MessageComponent } from './message/message.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MessageComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
